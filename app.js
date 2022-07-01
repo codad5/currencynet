dac4b04f8aaba4568f0777ceed1764c90e7da5eb
PTBYSR <paul.emechebe@gmail.com>
2022-07-01 04:45:45 +0100

    setting up server and tailwind css

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+const port = 3000
+
+app.set('view engine', 'ejs')
+// app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('pages/index')
+})
+
+app.get('/docs', (req, res) => {
+    res.render('pages/docs')
+})
+
+app.listen(port, () => {
+  console.log(`App listening at port ${port}`)
+})
