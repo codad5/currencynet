228529a9b6ddf5294cf7e9689807182359c28ee7
PTBYSR <paul.emechebe@gmail.com>
2022-08-01 20:04:32 +0100

    axios install for fetching github profiles

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
 const path = require('path');
 const port = 3000
 
+async function getPaul() {
+    try {
+      const response = await axios.get('https://api.github.com/users/PTBYSR');
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+async function getJerry() {
+    try {
+      const response = await axios.get('https://api.github.com/users/jerrygeorge360');
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+async function getChibueze() {
+    try {
+      const response = await axios.get('https://api.github.com/users/codad5');
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
 app.set('view engine', 'ejs')
-// app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'))
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
