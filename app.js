e359bdb6af8572cbb9961a1428e399bfed832af4
PTBYSR <paul.emechebe@gmail.com>
2022-08-01 23:26:21 +0100

    bug: axios not wokring

--- app.js
+++ app.js
@@ -21,18 +21,45 @@ const port = 3000
 //       console.error(error);
 //     }
 //   }
-async function getChibueze() {
-    try {
-      const response = await axios.get('https://api.github.com/users/codad5');
-      console.log('github fetched');
-    } catch (error) {
-      console.error('github no fetch');
-    }
 
-    return response
+// async function getUser() {
+//   let one = 'https://api.github.com/users/codad5'
+//   let two = 'https://api.github.com/users/PTBYSR'
+//   let three = 'https://api.github.com/users/jerrygeorge360'
+
+//   const requestOne = axios.get(one);
+//   const requestTwo = axios.get(two);
+//   const requestThree = axios.get(three);
+
+//   axios.all([requestOne, requestTwo, requestThree])
+//     .then(axios.spread((...responses) => {
+//     const responseOne = responses[0]
+//     const responseTwo = responses[1]
+//     const responesThree = responses[2]
+//     console.log(responseOne, responseTwo, responesThree);
+//     })).catch(errors => {
+//     console.log(errors)
+//     })
+// }
+
+
+// ----------------!!!!!!!!!!!!!!!!!----------help me debug section below
+
+async function getUser() {
+  try {
+    const data = await axios.get('https://api.github.com/users/codad5');
+    console.log('github fetched', data);
+  } catch (error) {
+    console.error('github no fetch');
   }
+}
 
-const data = getChibueze()
+getUser()
+
+
+// ----------------!!!!!!!!!!!!!!!!!----------help me debug section above
+
+
 app.set('view engine', 'ejs')
 // app.set('views', path.join(__dirname, 'views'))
 app.use(express.static('public'));
@@ -46,7 +73,7 @@ app.get('/docs', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-    res.render('pages/about')
+    res.render('pages/about', { data: data })
 })
 
 app.listen(port, () => {
