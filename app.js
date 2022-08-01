74c585e3dcc69d06ef3b1e9652a6145809d890c2
PTBYSR <paul.emechebe@gmail.com>
2022-08-01 23:23:16 +0100

    bug: axios not working

--- app.js
+++ app.js
@@ -2,33 +2,37 @@ const express = require('express')
 const axios = require('axios')
 const app = express()
 const path = require('path');
+const { response } = require('express');
 const port = 3000
 
-async function getPaul() {
-    try {
-      const response = await axios.get('https://api.github.com/users/PTBYSR');
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-async function getJerry() {
-    try {
-      const response = await axios.get('https://api.github.com/users/jerrygeorge360');
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+// async function getPaul() {
+//     try {
+//       const response = await axios.get('https://api.github.com/users/PTBYSR');
+//       console.log(response);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+// async function getJerry() {
+//     try {
+//       const response = await axios.get('https://api.github.com/users/jerrygeorge360');
+//       console.log(response);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
 async function getChibueze() {
     try {
       const response = await axios.get('https://api.github.com/users/codad5');
-      console.log(response);
+      console.log('github fetched');
     } catch (error) {
-      console.error(error);
+      console.error('github no fetch');
     }
+
+    return response
   }
 
+const data = getChibueze()
 app.set('view engine', 'ejs')
 // app.set('views', path.join(__dirname, 'views'))
 app.use(express.static('public'));
